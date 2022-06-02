714414739d0ebecc1fdde0ccc118d4c5ee422c2c
Juliano C L Santos <foxports@hotmail.com>
2022-06-02 00:06:30 -0300

    exibe image na tabela

--- ViewerFlags/ViewController.swift
+++ ViewerFlags/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var listFlags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loadFlags()
+        print(listFlags)
+    }
+    
+    
+    func loadFlags() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        var flags = try! fm.contentsOfDirectory(atPath: path)
+        flags.sort()
+
+        for flag in flags {
+            if flag.hasSuffix(".png") {
+                if let lastIndex = flag.lastIndex(of: "@") {
+                    let flagName = String(flag[..<lastIndex]) + ".png"
+                    if !listFlags.contains(flagName) {
+                        listFlags.append(flagName)
+                    }
+                }
+            }
+        }
     }
 
+    // Return size array
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listFlags.count
+    }
+
+    // return data of line namber
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Coutry", for: indexPath)
+        
+        if let indexName = listFlags[indexPath.row].lastIndex(of: ".")  {
+            let imageName = listFlags[indexPath.row]
+            let name = String(imageName[..<indexName])
+            cell.textLabel?.text = " - " + name
+        }
+        cell.imageView?.image = UIImage(named: listFlags[indexPath.row])
+        cell.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+
+        return cell
+    }
 
 }
 
